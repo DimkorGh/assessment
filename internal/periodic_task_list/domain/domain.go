4747f77c015a7a0b704eaf9aea572b0643364458
Dimko <rokmidapps@gmail.com>
2023-03-23 00:27:59 +0200

    Add ptList domain

---
+++ internal/periodic_task_list/domain/domain.go
@@ -0,0 +1,70 @@
+package domain
+
+import (
+	"time"
+)
+
+const (
+	oneHourPeriod  = "1h"
+	oneDayPeriod   = "1d"
+	oneMonthPeriod = "1mo"
+	oneYearPeriod  = "1y"
+)
+
+type PtListDomainInt interface {
+	AddPeriod(period string, t time.Time) time.Time
+	GetInvocationTimestamp(period string, t time.Time) time.Time
+}
+
+type PtListDomain struct {
+}
+
+func NewPtListDomain() *PtListDomain {
+	return &PtListDomain{}
+}
+
+func (pld *PtListDomain) AddPeriod(period string, t time.Time) time.Time {
+
+	switch period {
+	case oneHourPeriod:
+		t = t.Add(time.Hour)
+	case oneDayPeriod:
+		t = t.AddDate(0, 0, 1)
+	case oneMonthPeriod:
+		t = t.AddDate(0, 1, -1)
+		t = pld.getMonthLastDay(t)
+	default:
+		t = t.AddDate(1, 0, -1)
+		t = pld.getYearLastDay(t)
+	}
+
+	return t
+}
+
+func (pld *PtListDomain) GetInvocationTimestamp(period string, t time.Time) time.Time {
+	t = t.Add(time.Hour)
+	t = time.Date(t.Year(), t.Month(), t.Day(), t.Hour(), 0, 0, 0, t.Location())
+
+	switch period {
+	case oneMonthPeriod:
+		return pld.getMonthLastDay(t)
+	case oneYearPeriod:
+		return pld.getYearLastDay(t)
+	}
+
+	return t
+}
+
+func (pld *PtListDomain) getMonthLastDay(t time.Time) time.Time {
+	nextMonthFirstDay := time.Date(t.Year(), t.Month()+1, 1, t.Hour(), 0, 0, 0, t.Location())
+	lastMonthLastDay := nextMonthFirstDay.AddDate(0, 0, -1)
+
+	return lastMonthLastDay
+}
+
+func (pld *PtListDomain) getYearLastDay(t time.Time) time.Time {
+	nextYearFirstDay := time.Date(t.Year()+1, 1, 1, t.Hour(), 0, 0, 0, t.Location())
+	lastYearLastDay := nextYearFirstDay.AddDate(0, 0, -1)
+
+	return lastYearLastDay
+}
---
+++ internal/periodic_task_list/domain/domain_test.go
@@ -0,0 +1,134 @@
+package domain
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewPtListDomain(t *testing.T) {
+	t.Run("Test ptList domain constructor", func(t *testing.T) {
+		got := NewPtListDomain()
+
+		assert.Implements(t, new(PtListDomainInt), got)
+	})
+}
+
+func TestAddPeriod(t *testing.T) {
+	type args struct {
+		period string
+		t      time.Time
+		tZone  *time.Location
+	}
+	tests := []struct {
+		name string
+		args args
+		want time.Time
+	}{
+		{
+			name: "Add one hour period",
+			args: args{
+				period: oneHourPeriod,
+				t:      time.Date(2022, 1, 1, 21, 5, 6, 7, time.UTC),
+				tZone:  time.UTC,
+			},
+			want: time.Date(2022, 1, 1, 22, 5, 6, 7, time.UTC),
+		},
+		{
+			name: "Add one day period",
+			args: args{
+				period: oneDayPeriod,
+				t:      time.Date(2022, 1, 1, 21, 5, 6, 7, time.UTC),
+				tZone:  time.UTC,
+			},
+			want: time.Date(2022, 1, 2, 21, 5, 6, 7, time.UTC),
+		},
+		{
+			name: "Add one month period and get last day of month timestamp",
+			args: args{
+				period: oneMonthPeriod,
+				t:      time.Date(2022, 1, 1, 21, 5, 6, 7, time.UTC),
+				tZone:  time.UTC,
+			},
+			want: time.Date(2022, 1, 31, 21, 0, 0, 0, time.UTC),
+		},
+		{
+			name: "Add one year period and get last day of year timestamp",
+			args: args{
+				period: oneYearPeriod,
+				t:      time.Date(2022, 1, 1, 21, 5, 6, 7, time.UTC),
+				tZone:  time.UTC,
+			},
+			want: time.Date(2022, 12, 31, 21, 0, 0, 0, time.UTC),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			pld := NewPtListDomain()
+			got := pld.AddPeriod(tt.args.period, tt.args.t)
+
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func TestGetInvocationTimestamp(t *testing.T) {
+	type args struct {
+		period string
+		t      time.Time
+		tZone  *time.Location
+	}
+	tests := []struct {
+		name string
+		args args
+		want time.Time
+	}{
+		{
+			name: "Get invocation timestamp for one hour period",
+			args: args{
+				period: oneHourPeriod,
+				t:      time.Date(2022, 1, 2, 3, 4, 5, 6, time.UTC),
+				tZone:  time.UTC,
+			},
+			want: time.Date(2022, 1, 2, 4, 0, 0, 0, time.UTC),
+		},
+		{
+			name: "Get invocation timestamp for one day period",
+			args: args{
+				period: oneDayPeriod,
+				t:      time.Date(2022, 1, 2, 3, 4, 5, 6, time.UTC),
+				tZone:  time.UTC,
+			},
+			want: time.Date(2022, 1, 2, 4, 0, 0, 0, time.UTC),
+		},
+		{
+			name: "Get invocation timestamp for one month period",
+			args: args{
+				period: oneMonthPeriod,
+				t:      time.Date(2022, 1, 2, 3, 4, 5, 6, time.UTC),
+				tZone:  time.UTC,
+			},
+			want: time.Date(2022, 1, 31, 4, 0, 0, 0, time.UTC),
+		},
+		{
+			name: "Get invocation timestamp for one year period",
+			args: args{
+				period: oneYearPeriod,
+				t:      time.Date(2022, 1, 2, 3, 4, 5, 6, time.UTC),
+				tZone:  time.UTC,
+			},
+			want: time.Date(2022, 12, 31, 4, 0, 0, 0, time.UTC),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			pld := NewPtListDomain()
+			got := pld.GetInvocationTimestamp(tt.args.period, tt.args.t)
+
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
