416601d7ecd43712c62279eca9c023ad039299e7
Dimko <rokmidapps@gmail.com>
2023-03-23 00:39:36 +0200

    Add main function

---
+++ cmd/main/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"assessment/internal/cli"
+	"assessment/internal/core/wiring"
+)
+
+func main() {
+	argsReader := cli.NewArgsReader()
+	address, port, err := argsReader.ReadArgs()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	container := wiring.NewContainer()
+	container.InitializeDependencies(address, port)
+
+	osSignal := make(chan os.Signal)
+	signal.Notify(osSignal, os.Interrupt, syscall.SIGTERM)
+
+	go container.Server.Start()
+
+	<-osSignal
+	container.Server.Shutdown()
+}
