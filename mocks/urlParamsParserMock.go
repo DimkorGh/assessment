a1947a6dcf2ba4c66f8dc4b7549e526eb344b4ea
Dimko <rokmidapps@gmail.com>
2023-03-23 00:28:40 +0200

    Add url params parser mock

---
+++ mocks/urlParamsParserMock.go
@@ -0,0 +1,45 @@
+package mocks
+
+import (
+	"net/http"
+	"reflect"
+
+	"github.com/golang/mock/gomock"
+)
+
+// MockUrlParamsParserInt is a mock of UrlParamsParserInt interface.
+type MockUrlParamsParserInt struct {
+	ctrl     *gomock.Controller
+	recorder *MockUrlParamsParserIntMockRecorder
+}
+
+// MockUrlParamsParserIntMockRecorder is the mock recorder for MockUrlParamsParserInt.
+type MockUrlParamsParserIntMockRecorder struct {
+	mock *MockUrlParamsParserInt
+}
+
+// NewMockUrlParamsParserInt creates a new mock instance.
+func NewMockUrlParamsParserInt(ctrl *gomock.Controller) *MockUrlParamsParserInt {
+	mock := &MockUrlParamsParserInt{ctrl: ctrl}
+	mock.recorder = &MockUrlParamsParserIntMockRecorder{mock}
+	return mock
+}
+
+// EXPECT returns an object that allows the caller to indicate expected use.
+func (m *MockUrlParamsParserInt) EXPECT() *MockUrlParamsParserIntMockRecorder {
+	return m.recorder
+}
+
+// ParseUrlParams mocks base method.
+func (m *MockUrlParamsParserInt) ParseUrlParams(r *http.Request, data interface{}) error {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "ParseUrlParams", r, data)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// ParseUrlParams indicates an expected call of ParseUrlParams.
+func (mr *MockUrlParamsParserIntMockRecorder) ParseUrlParams(r, data interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "ParseUrlParams", reflect.TypeOf((*MockUrlParamsParserInt)(nil).ParseUrlParams), r, data)
+}
